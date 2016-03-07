e4fe20831e36103ae243afa0f8b03b374e3fc964
Isaac <isaacgoss14750@gmail.com>
2016-03-06 20:26:37 -0500

    Here's a little more progress

--- mips_soln/main.asm
+++ mips_soln/main.asm
@@ -3,6 +3,7 @@
 .data
 start_msg: .asciiz "How many rows would you like: "
 end_msg:   .asciiz "Here is the base address of the triangle: "
+soln_msg:  .asciiz "Here is the solution I came up with: "
 .text
 main:
         la      $a0,start_msg
@@ -25,6 +26,61 @@ main:
         li      $v0,34
         syscall
         
+        
+        
+##################################
+#      THE    BUSINESS           #
+##################################
+
+# $s0 <- minimum_sum
+# $s1 <- current_sum
+# $s2 <- meta_pointer
+# $s3 <- i
+# $s4 <- k
+
+
+        li      $s0,100         #minimum_sum = 100
+
+loop1:
+        
+        lw      $t0,($s2)       #while(meta[pointer]!=null) {
+        beqz    $t0,exit1
+        
+        
+        
+        li      $s3,0           # i = 0
+loop2:
+        add     $t3,$t0,$s3     #triangle* = meta*+i
+        lw      $t0,4($t3)      #while (tri[pointer+1]!=null) {
+        beqz    $t0,exit2
+        
+        li      $s1,0           #current_sum = 0
+        li      $s4,0
+loop3:
+        
+        lw      $t0,
+        
+        addi    $s4,$s4,4       #k++
+        j   loop3
+exit3:
+
+        addi     $s3,$s3,4      #i++
+        j   loop2
+        
+exit2:
+        
+        addi    $s1,$s1,4       #meta*++
+        j   loop1
+        
+exit1:
+        
+        
+        
+        
+        
+##################################
+#           THE END              #
+##################################
+        
         li      $v0,10
         syscall
-        
